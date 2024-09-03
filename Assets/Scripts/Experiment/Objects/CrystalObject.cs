71c031ee2bb69184c7b944e12255fe83d1c05bc7
GODkamiya <garlicgame@outlook.jp>
2024-09-03 21:43:15 +0900

    水晶のドラッグアンドドロップ可能

---
+++ Assets/Scripts/Experiment/Objects/CrystalObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        transform.position = mousePosition;
+    }
+}
