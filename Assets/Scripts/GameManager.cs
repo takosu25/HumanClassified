bbc52be3943636c04c7d0e13ebe89f127a8ae951
GODkamiya <garlicgame@outlook.jp>
2024-09-07 19:34:30 +0900

    GameManagerの追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject humanPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(humanPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
