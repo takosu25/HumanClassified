d8fb85955aa6f14f8e04d7ba5fd3eacfddc2510e
GODkamiya <garlicgame@outlook.jp>
2024-09-10 21:54:43 +0900

    AbilityFactoryを追加

---
+++ Assets/Scripts/AbilityFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AbilityResult
+{
+    Fire,Water,Nature,Litgh,Dark
+}
+public class AbilityFactory
+{
+    public static AbilityBase AbilityInstance(AbilityResult abilityResult)
+    {
+        switch (abilityResult)
+        {
+            case AbilityResult.Fire:
+                return new FireAbility();
+            case AbilityResult.Water:
+                return new WaterAbility();
+            case AbilityResult.Nature:
+                return new NatureAbility();
+            case AbilityResult.Litgh:
+                return new LightAbility();
+            case AbilityResult.Dark:
+                return new DarkAbility();
+            default:
+                throw new ArgumentException("属性を渡せと言っておるだろうが");
+
+        }
+    }
+}
