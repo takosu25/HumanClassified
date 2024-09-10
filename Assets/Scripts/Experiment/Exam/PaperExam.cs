06357c836164d96b25c9f20a7057951909b939be
KotaYoshioka <supakouta@gmail.com>
2024-09-10 21:22:57 +0900

    feat:PaperResultの定義

---
+++ Assets/Scripts/Experiment/Exam/PaperExam.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PaperResult
+{
+    BurnOut,Smoldering,NoFire
+}
+public class PaperExam : MonoBehaviour
+{
+}
