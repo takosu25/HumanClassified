31542a3be0911a80301f0f6630c992c1087bd051
KotaYoshioka <supakouta@gmail.com>
2024-09-10 21:31:30 +0900

    feat:火属性の追加

---
+++ Assets/Scripts/Ability/WaterAbility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAbility : AbilityBase
+{
+    public override string GetAbilityName()
+    {
+        return "水";
+    }
+}
