2a491871712c3df92bb089f93cf4e4fa4a2e205f
KotaYoshioka <supakouta@gmail.com>
2024-09-10 21:29:15 +0900

    木属性の追加

---
+++ Assets/Scripts/Ability/NatureAbility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NatureAbility : AbilityBase
+{
+    public override string GetAbilityName()
+    {
+        return "木";
+    }
+}
