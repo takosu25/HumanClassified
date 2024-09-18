89b1780df4509a2c7a4f2044939053c01c0f8b25
KotaYoshioka <supakouta@gmail.com>
2024-09-18 21:14:58 +0900

    feat:職業一覧画面にボタンが自動で配置される

---
+++ Assets/Scripts/UI/JobSelectionPanel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class JobSelectionPanel : MonoBehaviour
+{
+    [SerializeField]
+    GameObject buttonPrefab;
+    [SerializeField]
+    GameObject buttonListPanel;
+    void Start()
+    {
+        foreach(JobResult jobResult in Enum.GetValues(typeof(JobResult))){
+            var job = JobFactory.JobInstance(jobResult);
+            var button = Instantiate(buttonPrefab);
+            button.GetComponentInChildren<TextMeshProUGUI>().text = job.GetName();
+            button.transform.SetParent(buttonListPanel.transform, true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
