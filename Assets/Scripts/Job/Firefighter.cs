b2077e3b98e420b9d14c5724fd272c70e5a0e71e
GODkamiya <garlicgame@outlook.jp>
2024-09-18 22:16:08 +0900

    農家、消防士、料理人を追加

---
+++ Assets/Scripts/Job/Firefighter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firefighter : JobBase
+{
+    public override string GetName()
+    {
+        return "消防士";
+    }
+
+    public override bool Judge(JudgeJobOption option)
+    {
+        return MagicLevel.IsWeak(option.magicLevel) && option.abilityBase is WaterAbility;
+    }
+
+}
